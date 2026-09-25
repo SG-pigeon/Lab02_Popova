314bce444bc1b7b1203d7389eda432ec863c9199
SG-pigeon <sofiya.popova.1705@bk.ru>
2026-09-25 18:16:26 +0300

    Lab02: keyboard input with ReadLine

--- Program.cs
+++ Program.cs
@@ -1,48 +1,59 @@
-﻿// // Console.WriteLine("Границы целочисленных типов");
-// // Console.WriteLine($"byte:  {byte.MinValue}  {byte.MaxValue}");
-// // Console.WriteLine($"short:  {short.MinValue}  {short.MaxValue}");
-// // Console.WriteLine($"int:  {short.MinValue}  {int.MaxValue}");
-// // Console.WriteLine($"long:  {long.MinValue}  {long.MaxValue}");
+﻿// // // Console.WriteLine("Границы целочисленных типов");
+// // // Console.WriteLine($"byte:  {byte.MinValue}  {byte.MaxValue}");
+// // // Console.WriteLine($"short:  {short.MinValue}  {short.MaxValue}");
+// // // Console.WriteLine($"int:  {short.MinValue}  {int.MaxValue}");
+// // // Console.WriteLine($"long:  {long.MinValue}  {long.MaxValue}");
+
+// // // Console.WriteLine();
+// // // Console.WriteLine("Границы дробных типов");
+// // // Console.WriteLine($"float:  {float.MinValue}  {float.MaxValue}");
+// // // Console.WriteLine($"double:  {double.MinValue}  {double.MaxValue}");
+// // // Console.WriteLine($"decimal:  {decimal.MinValue}  {decimal.MaxValue}");
+
+// // // Console.WriteLine();
+// // // Console.WriteLine("Переполнение byte");
+
+// // // byte maxByte = 255;
+// // // byte overflowed = (byte)(maxByte + 1);
+// // // Console.WriteLine($"255 + 1 для byte = {overflowed}");
 
 // // Console.WriteLine();
-// // Console.WriteLine("Границы дробных типов");
-// // Console.WriteLine($"float:  {float.MinValue}  {float.MaxValue}");
-// // Console.WriteLine($"double:  {double.MinValue}  {double.MaxValue}");
-// // Console.WriteLine($"decimal:  {decimal.MinValue}  {decimal.MaxValue}");
+// // Console.WriteLine("char");
+
+// // char firstLetter = 'A';
+// // char separator = '-';
+// // int charAsNumber = firstLetter; 
+
+// // Console.WriteLine($"Символ: {firstLetter}, разделитель:{separator}");
+// // Console.WriteLine($"Код символа '{firstLetter}' в Unicode: {charAsNumber}");
+// // Console.WriteLine($"Табуляция:\tпосле таба");
+// // Console.WriteLine($"Перенос:\nпосле переноса");
 
 // // Console.WriteLine();
-// // Console.WriteLine("Переполнение byte");
+// // Console.WriteLine("decimal против double");
 
-// // byte maxByte = 255;
-// // byte overflowed = (byte)(maxByte + 1);
-// // Console.WriteLine($"255 + 1 для byte = {overflowed}");
+// // double priceDouble = 0.1 + 0.2;
+// // decimal priceDecimal = 0.1m + 0.2m;
 
-// Console.WriteLine();
-// Console.WriteLine("char");
-
-// char firstLetter = 'A';
-// char separator = '-';
-// int charAsNumber = firstLetter; 
-
-// Console.WriteLine($"Символ: {firstLetter}, разделитель:{separator}");
-// Console.WriteLine($"Код символа '{firstLetter}' в Unicode: {charAsNumber}");
-// Console.WriteLine($"Табуляция:\tпосле таба");
-// Console.WriteLine($"Перенос:\nпосле переноса");
+// // Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
+// // Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
 
 // Console.WriteLine();
-// Console.WriteLine("decimal против double");
+// Console.WriteLine("var");
 
-// double priceDouble = 0.1 + 0.2;
-// decimal priceDecimal = 0.1m + 0.2m;
+// var studentAge = 20;
+// var gpa = 4.75;
+// var fullName = "Смирнова А.С.";
 
-// Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
-// Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
+// Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gpa}");
 
 Console.WriteLine();
-Console.WriteLine("var");
+Console.WriteLine("Ввод текста");
 
-var studentAge = 20;
-var gpa = 4.75;
-var fullName = "Смирнова А.С.";
+Console.Write("Введите ваше имя: ");
+string enteredName = Console.ReadLine();
 
-Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gpa}");
+Console.Write("Введите название вашей группы: ");
+string enteredGroup = Console.ReadLine();
+
+Console.WriteLine($"Здравствуйте, {enteredName} из группы {enteredGroup}!");
